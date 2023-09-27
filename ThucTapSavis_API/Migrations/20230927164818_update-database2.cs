bf4555a21c0ff6d220d72e24bf9c4fbbe60638bf
Nguyễn Văn Thắng <thangnguyen1632003@gmail.com>
2023-09-27 23:48:58 +0700

    7 services đầu + sửa db

---
+++ ThucTapSavis_API/Migrations/20230927164818_update-database2.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ThucTapSavis_API.Migrations
+{
+    public partial class updatedatabase2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "CartItems");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "CartItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "CartItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "CartItems");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "CartItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
