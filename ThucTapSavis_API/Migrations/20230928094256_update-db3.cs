0aafc5f47b5e09d9e9e3dce9a772d70dfa2d72ce
Nguyễn Văn Thắng <thangnguyen1632003@gmail.com>
2023-09-28 16:43:19 +0700

    update-database

---
+++ ThucTapSavis_API/Migrations/20230928094256_update-db3.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ThucTapSavis_API.Migrations
+{
+    public partial class updatedb3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DiaChiCuThe",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Huyen",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RoleId",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tinh",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Xa",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DiaChiCuThe",
+                table: "Bills",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Huyen",
+                table: "Bills",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SDTNhan",
+                table: "Bills",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TenNguoiNhan",
+                table: "Bills",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tinh",
+                table: "Bills",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Xa",
+                table: "Bills",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_RoleId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DiaChiCuThe",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Huyen",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RoleId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Tinh",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Xa",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DiaChiCuThe",
+                table: "Bills");
+
+            migrationBuilder.DropColumn(
+                name: "Huyen",
+                table: "Bills");
+
+            migrationBuilder.DropColumn(
+                name: "SDTNhan",
+                table: "Bills");
+
+            migrationBuilder.DropColumn(
+                name: "TenNguoiNhan",
+                table: "Bills");
+
+            migrationBuilder.DropColumn(
+                name: "Tinh",
+                table: "Bills");
+
+            migrationBuilder.DropColumn(
+                name: "Xa",
+                table: "Bills");
+        }
+    }
+}
