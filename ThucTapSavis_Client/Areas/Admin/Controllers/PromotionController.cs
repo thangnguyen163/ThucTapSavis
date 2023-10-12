0e74f8254f73a0d4cd2db69b8f829efc5bb42ac8
Nguyễn Văn Thắng <thangnguyen1632003@gmail.com>
2023-10-12 15:24:00 +0700

    tuấn báo nhất nhà

---
+++ ThucTapSavis_Client/Areas/Admin/Controllers/PromotionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThucTapSavis_Client.Areas.Admin.Controllers
+{
+    public class PromotionController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
